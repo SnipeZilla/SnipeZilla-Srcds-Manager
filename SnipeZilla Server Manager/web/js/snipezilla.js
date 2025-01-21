fff0878a87a1a877b65d70f411d73a16b0837d5b
SnipeZilla <cedde@msn.com>
2025-01-20 19:30:06 -0500

    Add files via upload

--- SnipeZilla Server Manager/web/js/snipezilla.js
+++ SnipeZilla Server Manager/web/js/snipezilla.js
@@ -28,7 +28,7 @@ If not, see <http://www.gnu.org/licenses/>.
 */
 var Sz = {
     author: 'SnipeZilla',
-    version: 'Version: 1.2.5',
+    version: 'Version: 1.3.0',
     token: '',
     page: '',
     pid: 0,
